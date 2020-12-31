900aeded2298425126362839f11324dcdc5dbce1
uysaler <aysepernur@gmail.com>
2020-12-31 10:41:52 -0600

    added another comment

--- src/day1_/Practice1.java
+++ src/day1_/Practice1.java
@@ -4,6 +4,7 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("practice1");
         //added a comment
+        System.out.println("practice2");
 
     }
 }
